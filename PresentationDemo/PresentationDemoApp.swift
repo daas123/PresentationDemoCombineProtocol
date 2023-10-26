04e9bf3fe98fc16ffc6f5f271d06b4c974e1263d
saad <saadneosoft@gmail.com>
2023-10-26 10:23:24 +0530

    Initial Commit

---
+++ PresentationDemo/PresentationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationDemoApp.swift
+//  PresentationDemo
+//
+//  Created by Neosoft on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PresentationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
